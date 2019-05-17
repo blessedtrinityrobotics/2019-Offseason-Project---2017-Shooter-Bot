1114043bfb62f5144b3e27f78b6b9e3428f9e259
Dario Zaccagnino <37509360+Dariozac05@users.noreply.github.com>
2019-05-17 09:27:48 -0400

    Added code to Intake.java

--- 2017 Offseason Shooter Bot/src/main/java/frc/robot/subsystems/Intake.java
+++ 2017 Offseason Shooter Bot/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,1 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.PIDConstants;
+import frc.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+/**
+ * Add your docs here.
+ */
+public class Intake extends Subsystem {
+   
+  public VictorSPX leftIntakeMotor = new VictorSPX(RobotMap.leftIntakeMotorPort);
+  public VictorSPx rightIntakeMotor = new VictorSPX(RobotMap.rightIntakeMotorPort);
+  
+  public Intake() {
+    // Configure Intake Motors
+    leftIntakeMotor.setInverted(true); // Reverse direction
+    leftIntakeMotor.setNeutralMode(NeutralMode.Brake);
+    rightIntakeMotor.setInverted(false);
+    rightIntakeMotor.setNeutralMode(NeutralMode.Brake);
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new IntakeBalls());
+  }
+  
+  public void setIntakeSpeed(double speed) {
+    leftIntakeMotor.set(ControlMode.PercentOutput, speed);
+    rightIntakeMotor.set(ControlMode.PercentOutput, speed);
+  }
+  
+}
